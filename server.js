84ab94c4c5d3e628f7c113e066018d6f32783bc3
vervetwothree <jeepon35@gmail.com>
2022-12-11 07:33:02 -0500

    update 1

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
+if (process.env.NODE_ENV !=='production'){
+    require('dotenv').config()
+}
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
-require('dotenv').config()
 const express = require('express')
 const app = express()
-
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL)
-mongoose.set('strictQuery', true)
-
-const db = mongoose.connection 
-
-
-db.on('error', error => console.error(error))
-db.once('open', ()=> console.log('Connected to Mongoose'))
-
-
-
 const expressLayouts = require('express-ejs-layouts')
-const indexRouter = require('./routes/index')
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 
-
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended:false}))
 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+const db = mongoose.connection 
+mongoose.set('strictQuery', true)
+
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.use('/', indexRouter)
 
